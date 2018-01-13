16f1394f10542708ed1c9c389236761958b243fb
Alex Zhu <zhiqiang.zhu@mobisist.com>
2018-01-13 14:05:51 +0800

    initial commit

---
+++ src/js/inject.js
@@ -0,0 +1,14 @@
+(function () {
+    // document.body.style.backgroundColor="' + color + '";
+
+    // just place a div at top right
+    var div = document.createElement('div');
+    div.style.position = 'fixed';
+    div.style.top = 0;
+    div.style.right = 0;
+    div.textContent = 'Injected!';
+    document.body.appendChild(div);
+
+    // alert('inserted self... giggity');
+    window.scrollBy(0, 100);
+})();
---
+++ src/js/pop.js
@@ -0,0 +1,256 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, (tabs) => {
+        // chrome.tabs.query invokes the callback with a list of tabs that match the
+        // query. When the popup is opened, there is certainly a window and at least
+        // one tab, so we can safely assume that |tabs| is a non-empty array.
+        // A window can only have one active tab at a time, so the array consists of
+        // exactly one tab.
+        var tab = tabs[0];
+
+        // A tab is a plain object that provides information about the tab.
+        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        var url = tab.url;
+
+        // tab.url is only available if the "activeTab" permission is declared.
+        // If you want to see the URL of other tabs (e.g. after removing active:true
+        // from |queryInfo|), then the "tabs" permission is required to see their
+        // "url" properties.
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+
+        callback(url);
+    });
+
+    // Most methods of the Chrome extension APIs are asynchronous. This means that
+    // you CANNOT do something like this:
+    //
+    // var url;
+    // chrome.tabs.query(queryInfo, (tabs) => {
+    //   url = tabs[0].url;
+    // });
+    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+/**
+ * Change the background color of the current page.
+ *
+ * @param {string} color The new background color.
+ */
+function changeBackgroundColor(color) {
+    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
+    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
+    // into a page. Since we omit the optional first argument "tabId", the script
+    // is inserted into the active tab of the current window, which serves as the
+    // default.
+    chrome.tabs.executeScript({
+        file: 'js/inject.js'
+    });
+    // chrome.tts.speak(
+    //     '你好，世界. hello world', {'lang': 'zh-CN'});
+
+}
+
+/**
+ * Gets the saved background color for url.
+ *
+ * @param {string} url URL whose background color is to be retrieved.
+ * @param {function(string)} callback called with the saved background color for
+ *     the given url on success, or a falsy value if no color is retrieved.
+ */
+function getSavedBackgroundColor(url, callback) {
+    // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+    // for chrome.runtime.lastError to ensure correctness even when the API call
+    // fails.
+    chrome.storage.sync.get(url, (items) => {
+        callback(chrome.runtime.lastError ? null : items[url]);
+    });
+}
+
+/**
+ * Sets the given background color for url.
+ *
+ * @param {string} url URL for which background color is to be saved.
+ * @param {string} color The background color to be saved.
+ */
+function saveBackgroundColor(url, color) {
+    var items = {};
+    items[url] = color;
+    // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
+    // optional callback since we don't need to perform any action once the
+    // background color is saved.
+    chrome.storage.sync.set(items);
+}
+
+// This extension loads the saved background color for the current tab if one
+// exists. The user can select a new background color from the dropdown for the
+// current page, and it will be saved as part of the extension's isolated
+// storage. The chrome.storage API is used for this purpose. This is different
+// from the window.localStorage API, which is synchronous and stores data bound
+// to a document's origin. Also, using chrome.storage.sync instead of
+// chrome.storage.local allows the extension data to be synced across multiple
+// user devices.
+document.addEventListener('DOMContentLoaded', () => {
+    getCurrentTabUrl((url) => {
+        var start = document.getElementById('start');
+        var stop = document.getElementById('stop');
+        // Ensure the background color is changed and saved when the dropdown
+        // selection changes.
+        start.addEventListener('click', () => {
+            setupSpeechRecognition()
+        });
+    });
+});
+var recognizer;
+function setupSpeechRecognition() {
+    var recognitionMode = "Interactive", languageOptions = "zh-CN", key = "ec5a64ec335043a2924cee0f839c6e3b";
+    if (recognizer != null) {
+        RecognizerStop(window.SDK, recognizer);
+    }
+    recognizer = RecognizerSetup(window.SDK, recognitionMode, languageOptions, SDK.SpeechResultFormat['Simple'], key);
+}
+
+// Stop the Recognition.
+function RecognizerStop(SDK, recognizer) {
+    // recognizer.AudioSource.Detach(audioNodeId) can be also used here. (audioNodeId is part of ListeningStartedEvent)
+    recognizer.AudioSource.TurnOff();
+}
+
+function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
+
+    switch (recognitionMode) {
+        case "Interactive" :
+            recognitionMode = SDK.RecognitionMode.Interactive;
+            break;
+        case "Conversation" :
+            recognitionMode = SDK.RecognitionMode.Conversation;
+            break;
+        case "Dictation" :
+            recognitionMode = SDK.RecognitionMode.Dictation;
+            break;
+        default:
+            recognitionMode = SDK.RecognitionMode.Interactive;
+    }
+
+    var recognizerConfig = new SDK.RecognizerConfig(
+        new SDK.SpeechConfig(
+            new SDK.Context(
+                new SDK.OS(navigator.userAgent, "Browser", null),
+                new SDK.Device("SpeechSample", "SpeechSample", "1.0.00000"))),
+        recognitionMode,
+        language, // Supported languages are specific to each recognition mode. Refer to docs.
+        format); // SDK.SpeechResultFormat.Simple (Options - Simple/Detailed)
+
+
+    var useTokenAuth = false;
+
+    var authentication = function () {
+        if (!useTokenAuth)
+            return new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
+
+        var callback = function () {
+            var tokenDeferral = new SDK.Deferred();
+            try {
+                var xhr = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+                xhr.open('GET', '/token', 1);
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        tokenDeferral.Resolve(xhr.responseText);
+                    } else {
+                        tokenDeferral.Reject('Issue token request failed.');
+                    }
+                };
+                xhr.send();
+            } catch (e) {
+                window.console && console.log(e);
+                tokenDeferral.Reject(e.message);
+            }
+            return tokenDeferral.Promise();
+        }
+
+        return new SDK.CognitiveTokenAuthentication(callback, callback);
+    }();
+
+    var files = document.getElementById('filePicker').files;
+    if (!files.length) {
+        return SDK.CreateRecognizer(recognizerConfig, authentication);
+    } else {
+        return SDK.CreateRecognizerWithFileAudioSource(recognizerConfig, authentication, files[0]);
+    }
+}
+
+function RecognizerStart(SDK, recognizer) {
+    recognizer.Recognize((event) => {
+        /*
+         Alternative syntax for typescript devs.
+         if (event instanceof SDK.RecognitionTriggeredEvent)
+         */
+        switch (event.Name) {
+            case "RecognitionTriggeredEvent" :
+                UpdateStatus("Initializing");
+                break;
+            case "ListeningStartedEvent" :
+                UpdateStatus("Listening");
+                break;
+            case "RecognitionStartedEvent" :
+                UpdateStatus("Listening_Recognizing");
+                break;
+            case "SpeechStartDetectedEvent" :
+                UpdateStatus("Listening_DetectedSpeech_Recognizing");
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechHypothesisEvent" :
+                UpdateRecognizedHypothesis(event.Result.Text, false);
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechFragmentEvent" :
+                UpdateRecognizedHypothesis(event.Result.Text, true);
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechEndDetectedEvent" :
+                OnSpeechEndDetected();
+                UpdateStatus("Processing_Adding_Final_Touches");
+                console.log(JSON.stringify(event.Result)); // check console for other information in result
+                break;
+            case "SpeechSimplePhraseEvent" :
+                UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+                break;
+            case "SpeechDetailedPhraseEvent" :
+                UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+                break;
+            case "RecognitionEndedEvent" :
+                OnComplete();
+                UpdateStatus("Idle");
+                console.log(JSON.stringify(event)); // Debug information
+                break;
+            default:
+                console.log(JSON.stringify(event)); // Debug information
+        }
+    })
+        .On(() => {
+                // The request succeeded. Nothing to do here.
+            },
+            (error) => {
+                console.error(error);
+            });
+}
+
+
+function UpdateStatus(status) {
+    document.getElementById("status").innerHTML = status;
+}
